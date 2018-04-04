ce36d9805cf996a5f0f1ce619bc0e2e6d203c662
leunam <leunam.toshi@gmail.com>
2018-04-04 18:15:43 +0000

    Agregue friendly_url para tener url personalizada con el titulo del articulo, la url es unica

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,20 @@
 class Article < ActiveRecord::Base
+  before_save :anti_spam
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  validates_presence_of :title
+  validates_presence_of :body
+
+  def anti_spam
+    doc = Nokogiri::HTML::DocumentFragment.parse(body)
+
+    doc.css('a').each do |a|
+      a[:rel] = 'nofollow'
+      a[:target] = '_blank'
+    end
+
+    self.body = doc.to_s
+  end
+    
 end
